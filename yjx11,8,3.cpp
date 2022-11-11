3cebb86de86ff09876f81e547b25f9448912547f
siyuejiumeng <118001860+siyuejiumeng@users.noreply.github.com>
2022-11-12 00:10:53 +0800

    Add files via upload

---
+++ yjx11,8,3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>//求平均分，去掉最高分和最低分
+int main()
+{
+	int i,a[10],sum=0,max,min;
+	for(i=0;i<10;i++)
+	scanf("%d",&a[i]);
+	max=min=a[0];
+	for(i=0;i<10;i++)
+	{
+		sum+=a[i];
+		if(a[i]>max)
+		max=a[i];
+		if(a[i]<min)
+		min=a[i];
+	}
+	printf("%-6f",(sum-min-max)/8.0);
+}
