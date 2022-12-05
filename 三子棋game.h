ea30152154b7d1b09835375be69cfaa1f2fa8a1e
yjx <118001860+siyuejiumeng@users.noreply.github.com>
2022-12-05 10:15:46 +0800

    Create 三子棋game.h

---
+++ 三子棋game.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define ROW 3
+#define COL 3
+#include<stdlib.h>
+#include<time.h>
+
+void InitBoard(char board[ROW][COL],int row,int col);
+void DisplayBoard(char board[ROW][COL],int row,int col);
+void PlayerMove(char board[ROW][COL],int row,int col);
+void ComputerMove(char board[ROW][COL],int row,int col);
+char IsWin(char board[ROW][COL],int row,int col);
